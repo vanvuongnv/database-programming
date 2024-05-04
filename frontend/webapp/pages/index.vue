2b4978c4029bc29e07e3d4b411281c9b17c5dc28
vuongnguyenvan <vanvuong.nguyen0408@hotmail.com>
2024-05-04 14:46:15 +0700

    list student

--- frontend/webapp/pages/index.vue
+++ frontend/webapp/pages/index.vue
@@ -5,12 +5,66 @@
     <button type="button" class="btn btn-primary" @click="showMessage()">
       Click Me
     </button>
+
+    <div>
+      <h3>Full Name:</h3>
+      <input v-model="fullName" class="form-control" />
+
+      <p>Xin chào: {{ fullName }}</p>
+    </div>
+    <table class="table table-bordered">
+      <tr>
+        <td>Id</td>
+        <td>Name</td>
+        <td>Gender</td>
+      </tr>
+
+      <tr v-for="(item, index) in students" :key="index">
+        <td>
+          {{ item.id }}
+        </td>
+        <td>
+          {{ item.studentName }}
+        </td>
+        <td>
+          <span v-if="item.gender == 1"> Male </span>
+          <span v-else> Female </span>
+        </td>
+      </tr>
+    </table>
   </div>
 </template>
 
 <script>
 export default {
   name: 'IndexPage',
+  data() {
+    return {
+      fullName: '',
+      students: [
+        {
+          id: 1,
+          studentName: 'vuong',
+          gender: 1,
+        },
+        {
+          id: 2,
+          studentName: 'dieu',
+          gender: 2,
+        },
+        {
+          id: 3,
+          studentName: 'khue',
+          gender: 2,
+        },
+        {
+          id: 4,
+          studentName: 'an',
+          gender: 2,
+        },
+      ],
+    }
+  },
   methods: {
     showMessage() {
       alert('clicked')
