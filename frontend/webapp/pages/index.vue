e00b0cb1d0a3a0227546dddf0ec190f90b0d4e1c
vuongnguyenvan <vanvuong.nguyen0408@hotmail.com>
2024-05-04 14:20:36 +0700

    click event

--- frontend/webapp/pages/index.vue
+++ frontend/webapp/pages/index.vue
@@ -1,9 +1,23 @@
 <template>
-  <Tutorial />
+  <div class="wrapper container">
+    <h1>Hello VueJS</h1>
+    <p>I'm a new member of Nuxt/Vue</p>
+    <button type="button" class="btn btn-primary" @click="showMessage()">
+      Click Me
+    </button>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'IndexPage',
+  methods: {
+    showMessage() {
+      alert('clicked')
+    },
+  },
 }
 </script>
+
+<style scoped>
+</style>
