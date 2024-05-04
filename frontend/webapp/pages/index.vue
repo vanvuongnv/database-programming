4de9b26b08b86ff3eb52ac77bafcb7113c66f966
vuongnguyenvan <vanvuong.nguyen0408@hotmail.com>
2024-05-04 14:58:26 +0700

    form add

--- frontend/webapp/pages/index.vue
+++ frontend/webapp/pages/index.vue
@@ -5,33 +5,70 @@
     <button type="button" class="btn btn-primary" @click="showMessage()">
       Click Me
     </button>
+    <div class="row">
+      <div class="col-12 col-md-4">
+        <!--Form-->
+        <form>
+          <div class="form-group mb-3">
+            <label for="id" class="control-label"> Id </label>
+            <input v-model="dataSubmit.id" class="form-control" id="id" />
+          </div>
+          <div class="form-group mb-3">
+            <label for="fullName" class="control-label"> Full Name </label>
+            <input
+              v-model="dataSubmit.fullName"
+              class="form-control"
+              id="fullName"
+            />
+          </div>
+          <div class="form-group mb-3">
+            <label for="gender" class="control-label"> Gender </label>
+            <select
+              v-model="dataSubmit.gender"
+              id="gender"
+              class="form-control"
+            >
+              <option
+                v-for="(genderOption, index) in genderOptions"
+                :key="index"
+                :value="genderOption.id"
+              >
+                {{ genderOption.text }}
+              </option>
+            </select>
+          </div>
+        </form>
+      </div>
+      <div class="col-12 col-md-8">
+        <!--Table-->
+        <table class="table table-bordered">
+          <tr>
+            <td>Id</td>
+            <td>Name</td>
+            <td>Gender</td>
+          </tr>
 
+          <tr v-for="(item, index) in students" :key="index">
+            <td>
+              {{ item.id }}
+            </td>
+            <td>
+              {{ item.studentName }}
+            </td>
+            <td>
+              <span v-if="item.gender == 1"> Male </span>
+              <span v-else> Female </span>
+            </td>
+          </tr>
+        </table>
+      </div>
+    </div>
     <div>
       <h3>Full Name:</h3>
       <input v-model="fullName" class="form-control" />
 
       <p>Xin chào: {{ fullName }}</p>
     </div>
-    <table class="table table-bordered">
-      <tr>
-        <td>Id</td>
-        <td>Name</td>
-        <td>Gender</td>
-      </tr>
-
-      <tr v-for="(item, index) in students" :key="index">
-        <td>
-          {{ item.id }}
-        </td>
-        <td>
-          {{ item.studentName }}
-        </td>
-        <td>
-          <span v-if="item.gender == 1"> Male </span>
-          <span v-else> Female </span>
-        </td>
-      </tr>
-    </table>
   </div>
 </template>
 
@@ -62,6 +99,15 @@ export default {
           studentName: 'an',
           gender: 2,
         },
+      ],
+      dataSubmit: {
+        id: null,
+        fullName: '',
+        gender: null,
+      },
+      genderOptions: [
+        { id: 1, text: 'Male' },
+        { id: 2, text: 'Female' },
       ],
     }
   },
